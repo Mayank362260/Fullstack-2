47e076d2c4981b256d9faf5a2034e54d791cd6e9
MAyank SIngla <23BAI70316@cuchd.in>
2026-02-23 22:24:13 +0530

    Fixed base path for Vercel

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-                            base: "/Experiment-4/"
+  base: './',   // 🔥 ADD THIS LINE
 })
